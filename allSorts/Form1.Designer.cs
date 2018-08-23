d64a9b3a25a2d82f103d8a4b7a43f394dc3e9e46
WolfyDen <wolfyden@gmail.com>
2018-08-23 23:31:05 +0200

    yxsf

--- allSorts/Form1.Designer.cs
+++ allSorts/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace allSorts
+﻿using System.Windows.Forms;
+
+namespace allSorts
 {
 	partial class Form1
 	{
@@ -31,6 +33,11 @@
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.tb_End = new System.Windows.Forms.TrackBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.tb_End)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// comboBox1
@@ -38,49 +45,111 @@
 			this.comboBox1.FormattingEnabled = true;
 			this.comboBox1.Items.AddRange(new object[] {
             "Bubble Sort",
+            "Bubble Sort v2",
             "Quicksort",
             "Pigeonhole Sort"});
-			this.comboBox1.Location = new System.Drawing.Point(12, 12);
+			this.comboBox1.Location = new System.Drawing.Point(12, 532);
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(147, 21);
 			this.comboBox1.TabIndex = 0;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(165, 12);
+			this.button1.Location = new System.Drawing.Point(165, 532);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 21);
 			this.button1.TabIndex = 1;
-			this.button1.Text = "button1";
+			this.button1.Text = "Start";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// comboBox2
 			// 
 			this.comboBox2.FormattingEnabled = true;
-			this.comboBox2.Location = new System.Drawing.Point(12, 39);
+			this.comboBox2.Items.AddRange(new object[] {
+            "Fix number array (0 to end)",
+            "Fix number array worst case (end to 0)",
+            "Fix string Array (aaaa to end)",
+            "Random number array (start to end)",
+            "Random number array worst case (end to start)"});
+			this.comboBox2.Location = new System.Drawing.Point(12, 559);
 			this.comboBox2.Name = "comboBox2";
 			this.comboBox2.Size = new System.Drawing.Size(147, 21);
 			this.comboBox2.TabIndex = 2;
+			// 
+			// tb_End
+			// 
+			this.tb_End.AutoSize = false;
+			this.tb_End.Location = new System.Drawing.Point(53, 586);
+			this.tb_End.Maximum = 9999;
+			this.tb_End.Minimum = 10;
+			this.tb_End.Name = "tb_End";
+			this.tb_End.Size = new System.Drawing.Size(106, 24);
+			this.tb_End.TabIndex = 3;
+			this.tb_End.TickFrequency = 0;
+			this.tb_End.TickStyle = System.Windows.Forms.TickStyle.None;
+			this.tb_End.Value = 20;
+			this.tb_End.ValueChanged += new System.EventHandler(this.tb_End_ValueChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(165, 592);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(19, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "20";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(165, 562);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(189, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Bubble sort with Random number array";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 592);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Length: ";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.comboBox2);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(800, 619);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.tb_End);
+			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.tb_End)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ComboBox comboBox1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.ComboBox comboBox2;
+		private ComboBox comboBox1;
+		private Button button1;
+		private ComboBox comboBox2;
+		private TrackBar tb_End;
+		private Label label2;
+		private Label label1;
+		private Label label3;
 	}
 }
 
--- allSorts/Form1.cs
+++ allSorts/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,207 @@ namespace allSorts
 {
 	public partial class Form1 : Form
 	{
+		int arrayType = 0;
+		int[] intArray = new int[] { 0 };
+		int arrayLength = 20;
+		int from = 0;
+		int to = 0;
+		int len = -1;
+		item[] lst2;
+		int comparisons = 0;
+		int arrayAccesses = 0;
+		StringFormat sf = new StringFormat() { Alignment = StringAlignment.Center };
+
 		public Form1()
 		{
 			InitializeComponent();
+			Load += Form1_Load;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.SelectedIndex = 0;
+
+			DoubleBuffered = true;
+
+			this.SetStyle(
+				ControlStyles.AllPaintingInWmPaint |
+				ControlStyles.UserPaint |
+				ControlStyles.OptimizedDoubleBuffer,
+				true);
+			
+
+			/*
+			typeof(Form).InvokeMember("DoubleBuffered",
+			   BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+			   null, this, new object[] { true });
+			  */ 
+		}
+
+		protected override void OnPaint(PaintEventArgs e) { }
+
+		public void bubbleSort(int[] inp)
+		{
+			bool run = true;
+			int i = 0;
+			int tmp = 0;
+			myDelegate md = new myDelegate(writeToScreen);
+			int end = -1;
+			len = inp.Length;
+
+			while (run)
+			{
+				if (len > i + 1)
+				{
+					from = inp[i];
+					to = inp[i + 1];
+					comparisons++;
+					if (inp[i] > inp[i + 1])
+					{
+						arrayAccesses++;
+						end = i;
+						tmp = inp[i];
+						inp[i] = inp[i + 1];
+						inp[i + 1] = tmp;
+
+					}
+
+					Thread.Sleep(100);
+					this.Invoke(md);
+					i++;
+					if(arrayType == 1)
+					{
+						if (i + 1 == len) { len--; }
+					}
+				}
+				else
+				{
+					if (end == 0)
+					{
+						run = false; MessageBox.Show("OK");
+					}
+					else
+					{
+						end = 0;
+					}
+					i = 0;
+				}
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			arrayAccesses = comparisons = 0;
+			Random r0 = new Random();
+			List<item> lst = new List<item>();
+			
+			int min = 0;
+			for (int i = 0; i < 20; i++)
+			{
+				min = new Random(((int)((float)Environment.TickCount / Math.PI) * r0.Next())).Next(min, min + 255);
+				lst.Add(new item() { key = i + 1, number = min });
+				Thread.Sleep(10);
+			}
+			lst2 = lst.ToArray();
+			shuffle(lst2);
+			intArray = lst2.Select(x => x.number).ToArray();
+
+			arrayType = comboBox1.SelectedIndex;
+
+			Thread t = new Thread(
+				() => bubbleSort(intArray)
+			);
+			t.Start();
+
+		}
+
+		public void shuffle(item[] arr)
+		{
+			Random r0 = new Random();
+			int i = arr.Length - 1;
+			while (i > 1)
+			{
+				int r = new Random(((int)((float)Environment.TickCount / Math.PI) * r0.Next())).Next(0, i);
+				var v1 = arr[i];
+				arr[i] = arr[r];
+				arr[r] = v1;
+				i--;
+			}
+		}
+
+		public delegate void myDelegate();
+		public void writeToScreen()
+		{
+			int ii = 0;
+			using (Graphics g = CreateGraphics())
+			{
+				g.Clear(Color.AliceBlue);
+				
+
+				Color c = Color.Black;
+				foreach (int i in intArray)
+				{
+					c = Color.Black;
+
+					if (i == from)
+					{
+						c = Color.Red;
+					}
+					else if (i == to || ii >= len)
+					{
+						c = Color.Green;
+					}
+					item it = lst2.Where(x => x.number == i).ToArray()[0];
+					g.DrawString(it.number.ToString(), this.Font, new SolidBrush(c), new Point(30 + (ii * 30), 30), sf);
+					g.DrawString(it.key.ToString(), this.Font, new SolidBrush(c), new Point(30 + (ii * 30), 50), sf);
+
+					g.FillRectangle(Brushes.ForestGreen, new Rectangle(new Point(30 + (ii * 30) - 6, 65), new Size(13, it.key * 10)));
+
+					g.DrawString("Array accesses: " +	arrayAccesses,	this.Font, Brushes.Black, new Point(50, 300));
+					g.DrawString("Comparisons: " +		comparisons,	this.Font, Brushes.Black, new Point(50, 330));
+
+					g.Flush();
+
+					ii++;
+				}
+			}
+		}
+
+		private void tb_End_ValueChanged(object sender, EventArgs e)
+		{
+			arrayLength = tb_End.Value;
+			label2.Text = arrayLength + "";
 		}
 	}
+
+	public struct item
+	{
+		public int key;
+		public int number;
+	}
+
+	public class myPanel : Panel
+	{
+		public myPanel()
+		{
+			DoubleBuffered = true;
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			UpdateStyles();
+		}
+		
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+				return cp;
+			}
+		}
+		
+
+	}
+
 }
